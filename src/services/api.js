1a6d04f9bb2788b56a7b73291df135764c152bd5
Roger Marques <marquesroger226@gmail.com>
2022-10-07 20:52:47 -0300

    teste

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,25 @@
 export async function getCategories() {
-  // Implemente aqui
+  const urlCategories = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const request = await fetch(urlCategories);
+  const response = await request.json();
+  console.log('ola');
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (query) {
+    const urlQuerry = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+    const request = await fetch(urlQuerry);
+    const response = await request.json();
+
+    return response;
+  }
+
+  const urlCategoryId = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const request = await fetch(urlCategoryId);
+  const response = await request.json();
+
+  return response;
 }
 
 export async function getProductById() {
