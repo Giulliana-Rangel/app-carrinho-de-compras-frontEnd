e9971822d25ac87e224e66de7b7709d9e9a3232b
Giulliana Rangel <seuemail@exemplo.br>
2022-10-08 10:00:22 -0300

    test

--- src/services/api.js
+++ src/services/api.js
@@ -23,7 +23,6 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
 
   return response;
 }
-
 export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
